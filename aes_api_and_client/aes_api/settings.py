86207c40d4b52c55cae09e52117c5ecad9e1cb01
vshah <vshah@ncsu.edu>
2019-05-17 14:36:12 -0700

    Preparing for Heroku deployment

--- aes_api_and_client/aes_api/settings.py
+++ aes_api_and_client/aes_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$jb+yt3@-_5rjj8j74kbj&93)pbh8ievm_^$_(qufhp$cbblw4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+"""
+ToDo: Turn off debug, modify allowed hosts
+ToDo: Modify cors in urls.py
+"""
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
